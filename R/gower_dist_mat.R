18ea161cdb94cad5da8a9fc1bf3d403ffd7868ae
Aaron Brown <abrown52@tamu.edu>
2024-11-05 20:54:06 -0600

    Initialized gower distance matrix function R file

---
+++ R/gower_dist_mat.R
@@ -0,0 +1,22 @@
+#' Gowers Distance Matrix
+#'
+#' @param X A n x p dataframe object (allows for mixed data types) or a matrix object (single data type)
+#' @param var_type_vec p x 1 vector with elements indicating the data type of each variable of X
+#' @param var_weight_vec p x 1 vector with elements indicating variable weights applied in Gower's Distance calculations
+#'
+#' @return A matrix
+#' \item{gower_mat}{ A n x n matrix representing the pairwise Gower's Distances between observations of X }
+#' @export
+#'
+#' @examples
+#'
+#' ##### Example 1
+#'
+#' #####################
+#' ##### Example 2
+#'
+Gower_Dist_Mat <- function(X, var_type_vec, var_weight_vec){
+
+
+  return(gower_mat)
+}
